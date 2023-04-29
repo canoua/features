6594ae00b8bb531b6a27e4f5b4ed19c5554ee465
Andrew-Karyaka <akaryaka1997@gmail.com>
2023-04-30 01:41:14 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+ const input = document.querySelectorAll('.input');
+
+input.forEach(function(item) {  
+  item.addEventListener('focus', function() {
+    let id = item.id;
+    let label = document.querySelector(`[for="${id}"]`);
+    label.classList.add('label-top');
+  })
+
+  item.addEventListener('focusout', function() {
+    let id = item.id;
+    let label = document.querySelector(`[for="${id}"]`);
+    if(item.value == '') {
+      label.classList.remove('label-top');
+    }
+  })
+})
