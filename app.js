cd84b9d69455a00e31329eb1eef4f4188ba34cba
akaryaka <a.karyaka.2dit@gmail.com>
2023-05-09 12:38:21 +0600

    first commit preloader

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-const input = document.querySelectorAll('.input');
-
-input.forEach(function(item) {  
-  item.addEventListener('focus', function() {
-    let id = item.id;
-    let label = document.querySelector(`[for="${id}"]`);
-    label.classList.add('label-top');
-  })
-
-  item.addEventListener('focusout', function() {
-    let id = item.id;
-    let label = document.querySelector(`[for="${id}"]`);
-    if(item.value == '') {
-      label.classList.remove('label-top');
-    }
-  })
-})
+window.onload= function(){
+  let preloader = document.getElementById('preloader');
+  preloader.style.display = 'none';
+}
