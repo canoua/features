04391a1d7e2a5106ca8a6f30390588ca78392723
Andrew-Karyaka <akaryaka1997@gmail.com>
2023-07-11 17:06:07 +0600

    refactoring: slides

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let sources = [
   'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg', 
   'https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_484-l33iFhLTGqnFOSJHfx6OVpzWIkFER6t4jPo&s'
+  'https://imgv3.fotor.com/images/blog-cover-image/part-blurry-image.jpg'
 ];
 
 let currentIndex = 0;
@@ -9,6 +9,7 @@ let slide = document.querySelector('.slide__image');
 
 const arrowLeft = document.querySelector('.arrow-left');
 const arrowRight = document.querySelector('.arrow-right');
+const dots = document.querySelectorAll('.pagination__dot');
 
 arrowLeft.addEventListener('click', function() {
   if(currentIndex!=0) {
