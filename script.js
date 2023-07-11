48b0b678459a6b1686792f56712a3e46c4754554
Andrew-Karyaka <akaryaka1997@gmail.com>
2023-07-11 17:29:50 +0600

    refactoring functions

--- script.js
+++ script.js
@@ -3,32 +3,50 @@ let sources = [
   'https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg',
   'https://imgv3.fotor.com/images/blog-cover-image/part-blurry-image.jpg'
 ];
-
 let currentIndex = 0;
 let slide = document.querySelector('.slide__image');
-
 const arrowLeft = document.querySelector('.arrow-left');
 const arrowRight = document.querySelector('.arrow-right');
 const dots = document.querySelectorAll('.pagination__dot');
 
+//меняем ссылку картинки
+function slideChange() {
+  slide.src = sources[currentIndex];
+}
 
+//меняем слайд при клике на точки пагинации
 for(let i=0; i<dots.length; i++) {
   dots[i].addEventListener('click', function() {
     currentIndex = i;
-    slide.src = sources[currentIndex];
+    slideChange();
   }) 
 }
 
+function currentIndexChange(arrow) {
+  switch (arrow) {
+    case arrowLeft:
+      currentIndex = --currentIndex;
+      slideChange();
+      break;
+    case arrowRight:
+      currentIndex = ++currentIndex;
+      slideChange();
+      break;
+    default:
+      break;
+  }
+}
+
+//кнопка назад
 arrowLeft.addEventListener('click', function() {
   if(currentIndex!=0) {
-    currentIndex = --currentIndex;
-    slide.src = sources[currentIndex];
+    currentIndexChange(arrowLeft);
   }
 })
 
+//кнопка вперед
 arrowRight.addEventListener('click', function() {
   if(currentIndex != sources.length-1) {
-    currentIndex = ++currentIndex;
-    slide.src = sources[currentIndex];
+    currentIndexChange(arrowRight);
   }
 })
